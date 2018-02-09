0f55a37aee69517091cd2c33b1ab83abe2b1478a
Vojnovits Csaba <Vojnovits Csaba@CSABI>
2018-02-09 21:33:08 +0100

    commit Examples

--- 01_03_ShapesCollection/shapescollection/MainApp.java
+++ 01_03_ShapesCollection/shapescollection/MainApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new GenericXmlApplicationContext("beans.xml");
+		ApplicationContext context = new GenericXmlApplicationContext("beansofshapescollection.xml");
 		PrintShapes printShapes = context.getBean(PrintShapes.class);
 		printShapes.printShapes();
 	}
