abaceb1f1b871a1aae70747ffe20b5d6328028c0
Vojnovits Csaba <Vojnovits Csaba@CSABI>
2018-02-10 16:39:01 +0100

    commit Examples

--- 01_03_ShapesCollection/shapescollection/MainApp.java
+++ 01_03_ShapesCollection/shapescollection/MainApp.java
@@ -9,6 +9,6 @@ public class MainApp {
 		ApplicationContext context = new GenericXmlApplicationContext("beansofshapescollection.xml");
 		PrintShapes printShapes = context.getBean(PrintShapes.class);
 		printShapes.printShapes();
-		
+		((GenericXmlApplicationContext)context).close();
 	}
 }
